beb3c46ef1ecd782ab5eefdc0475c62e9ab3a991
Marcelobbl <produtormarcelo@hotmail.com>
2020-05-14 22:40:25 -0300

    Criacao, delete e busca no postman

--- src/main/java/RestFullApplication/controller/SoldadoController.java
+++ src/main/java/RestFullApplication/controller/SoldadoController.java
@@ -1,13 +1,13 @@
 package RestFullApplication.controller;
 
 import RestFullApplication.dto.Soldado;
+import RestFullApplication.request.SoldadoEditRequest;
 import RestFullApplication.service.SoldadoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/soldado")
@@ -15,13 +15,34 @@ public class SoldadoController {
 
     private SoldadoService soldadoService;
 
-    public SoldadoController(SoldadoService soldadoService){
+    public SoldadoController(SoldadoService soldadoService) {
         this.soldadoService = soldadoService;
     }
 
     @GetMapping("/{cpf}")
     public ResponseEntity<Soldado> responseEntity(@PathVariable() String cpf) {
-        Soldado soldado =soldadoService.buscarSoldado(cpf);
+        Soldado soldado = soldadoService.buscarSoldado(cpf);
         return ResponseEntity.status(HttpStatus.OK).body(soldado);
+    }
+
+    @PostMapping
+    public ResponseEntity criarSoldado(@RequestBody Soldado soldado) {
+        soldadoService.criarSoldado(soldado);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    @PutMapping("/{cpf}")
+    public ResponseEntity editarSoldado(@PathVariable() String cpf, @RequestBody SoldadoEditRequest soldadoEditRequest){
+        soldadoEditRequest.alterarSoldado(cpf, soldadoEditRequest);
+        return ResponseEntity.ok().build();
+    }
+    @DeleteMapping("/{cpf}")
+    public ResponseEntity deleteSoldado(@PathVariable String cpf){
+        soldadoService.deletarSoldado(cpf);
+        return ResponseEntity.ok().build();
+    }
+    @GetMapping()
+    public ResponseEntity<List<Soldado>> responseEntity() {
+        List<Soldado> soldados = soldadoService.todosSoldados();
+        return ResponseEntity.status(HttpStatus.OK).body(soldados);
     }
 }
--- src/main/java/RestFullApplication/service/SoldadoService.java
+++ src/main/java/RestFullApplication/service/SoldadoService.java
@@ -3,6 +3,10 @@ package RestFullApplication.service;
 import RestFullApplication.dto.Soldado;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Service
 public class SoldadoService {
 
@@ -13,5 +17,28 @@ public class SoldadoService {
         soldado.setRaca("Elfo");
         soldado.setArma("Arco e Flexa");
         return soldado;
+    }
+
+    public void criarSoldado(Soldado soldado){
+
+    }
+
+    public void deletarSoldado(String cpf) {
+
+    }
+
+    public List<Soldado> todosSoldados() {
+        Soldado soldado1 = new Soldado();
+        soldado1.setCpf("123456789");
+        soldado1.setNome("Ricardo");
+        soldado1.setRaca("Humano");
+        soldado1.setArma("Bazuca");
+        Soldado soldado2 = new Soldado();
+        soldado2.setCpf("21436576");
+        soldado2.setNome("Fábio");
+        soldado2.setRaca("Elfo");
+        soldado2.setArma("Metralhadora");
+
+        return Arrays.asList(soldado1,soldado2);
     }
 }
